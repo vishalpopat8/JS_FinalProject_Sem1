1a331e56a56c69f124ed4eea9e1f34ae5a97700e
Vishal <vishalpopat8@gmail.com>
2023-04-09 00:21:38 -0400

    v

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
+
 $(document).ready(() => {
-    var prevScrollpos = $(window).scrollTop();
+    let prevScrollpos = $(window).scrollTop();
     $(window).scroll(() => {
         var currentScrollPos = $(window).scrollTop();
         if (prevScrollpos > currentScrollPos) {
             $(".header-nav").css("top", "0");
         } else {
-            $(".header-nav").css("top", "-50px");
+            $(".header-nav").css("top", "-80px");
         }
         prevScrollpos = currentScrollPos;
     });
