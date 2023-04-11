14169fcce17e97aaf21ad926e8190cb7e64939fb
Vishal <vishalpopat8@gmail.com>
2023-04-10 20:13:17 -0400

    v

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,62 @@
 // hiding navbar while scrolling
 $(document).ready(() => {
-    let prevScrollpos = $(window).scrollTop();
-    $(window).scroll(() => {
-        var currentScrollPos = $(window).scrollTop();
-        if (prevScrollpos > currentScrollPos) {
-            $(".header-nav").css("top", "0");
-        } else {
-            $(".header-nav").css("top", "-80px");
-        }
-        prevScrollpos = currentScrollPos;
-    });
+  let prevScrollpos = $(window).scrollTop();
+  $(window).scroll(() => {
+    var currentScrollPos = $(window).scrollTop();
+    if ($("main section,footer").css("opacity") == 1) {
+      if (prevScrollpos > currentScrollPos) {
+        $(".header-nav").css("top", "0");
+      } else {
+        $(".header-nav").css("top", "-80px");
+      }
+    }
+    prevScrollpos = currentScrollPos;
+  });
 });
 
 // slider plugin
 $(".main-carousel").slick({
-    arrows: false,
-    dots: true,
-    customPaging : function(slider, i) {
-        return '<button class="slider-dot"></button>';
-    },
+  arrows: false,
+  dots: true,
+  customPaging: function (slider, i) {
+    return '<button class="slider-dot"></button>';
+  },
 });
 
 // button hover
 $(document).ready(() => {
-    $(".cell-content button").hover(
-        () => {
-            $(".cell-content button").css("background", "#fcaf17");
-            $(".cell-content button").css("border-color", "#fcaf17");
-            $(".cell-content button").css("color", "#161616");
-        },
-        () => {
-            $(".cell-content button").css("background", "transparent");
-            $(".cell-content button").css("border-color", "white");
-            $(".cell-content button").css("color", "white");
-        }
-    );
+  $(".cell-content button").hover(
+    () => {
+      $(".cell-content button").css("background", "#fcaf17");
+      $(".cell-content button").css("border-color", "#fcaf17");
+      $(".cell-content button").css("color", "#161616");
+    },
+    () => {
+      $(".cell-content button").css("background", "transparent");
+      $(".cell-content button").css("border-color", "white");
+      $(".cell-content button").css("color", "white");
+    }
+  );
 });
 
-// profile icon tooltip widget
+// sidebar toggle
 $(document).ready(() => {
-    
+  $(".hamb-icon").click(() => {
+    if ($("main section,footer").css("opacity") == 1) {
+      $("main section,footer").animate({ opacity: "0.5" });
+    } else {
+      $("main section,footer").animate({ opacity: "1" });
+    }
+    $("#sidebar").toggle("slide", () => {});
+
+    $(".hamb-bars div:first-child").animate({ opacity: "toggle" }, 30);
+    $(".hamb-bars div:last-child").animate({ opacity: "toggle" }, 30);
+  });
+});
+
+// sidebar toggle
+$(document).ready(() => {
+  $(".profile-logo a:first-child").click(() => {
+    $("nav").fadeToggle("fast");
+  });
 });
