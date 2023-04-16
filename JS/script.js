82443cea65eb46a4bb75b5cf974dcec1c9881e4f
Vishal <vishalpopat8@gmail.com>
2023-04-16 12:35:30 -0400

    v

--- JS/script.js
+++ JS/script.js
@@ -17,10 +17,37 @@ $(document).ready(() => {
 	});
 });
 
-// slider plugin
+// slider plugin home page
+$(".videos-reddead-content").slick({
+	arrows: true,
+	infinite: false,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	swipe: false,
+	prevArrow: $('.left-arrow'),
+	nextArrow: $('.right-arrow'),
+
+});
+$(document).ready(function () {
+	$('.videos-reddead-content').on('init', function (event, slick) {
+		// Get the previous and next arrow buttons
+		var prevArrow = $(slick.$prevArrow);
+		var nextArrow = $(slick.$nextArrow);
+
+		// Check if there are no previous or next slides
+		if (slick.slideCount <= slick.options.slidesToShow) {
+			// If there are no previous or next slides
+			prevArrow.addClass('slick-disabled');
+			nextArrow.addClass('slick-disabled');
+		}
+	});
+});
+
+// slider plugin videos page
 $(".main-carousel").slick({
 	arrows: false,
 	dots: true,
+	autoplay: true,
 	customPaging: function () {
 		return '<button class="slider-dot"></button>';
 	},
@@ -45,23 +72,22 @@ $(document).ready(() => {
 // sidebar toggle
 $(document).ready(() => {
 	$(".hamb-bars").click(() => {
-		if ($("main section,footer").css("opacity") == 1) {
-			$("main section,footer,.games-list").animate({ opacity: "0.5" });
+		if ($("main section,footer,.games-list,.games-search").css("opacity") == 1) {
+			$("main section,footer,.games-list,.games-search").animate({ opacity: "0.5" });
 		} else {
-			$("main section,footer,.games-list").animate({ opacity: "1" });
+			$("main section,footer,.games-list,.games-search").animate({ opacity: "1" });
 		}
 		$("#sidebar").toggle("slide", () => {
 			if ($("#sidebar").css("display") == "block") {
-				$("main section,footer,.games-list").css("pointer-events", "none");
+				$("main section,footer,.games-list,.games-search").css("pointer-events", "none");
 			} else {
-				$("main section,footer,.games-list").css("pointer-events", "unset");
+				$("main section,footer,.games-list,.games-search").css("pointer-events", "unset");
 			}
 		});
 
 		$(".hamb-bars div:first-child").animate({ opacity: "toggle" }, 300);
 		$(".hamb-bars div:last-child").animate({ opacity: "toggle" }, 300);
-	});
-
+	});	
 });
 
 // current page link css
@@ -133,34 +159,34 @@ $(document).ready(() => {
 });
 
 
-$( function() {
-    var availableTags = [
-      "Grand Theft Auto: Mission Pack #1 - London 1969",
-      "Grand Theft Auto: Mission Pack #2 - London 1961",
-      "Monster Truck Madness 64",
-      "Grand Theft Auto 2",
-      "Earthworm Jim 3D",
-      "Thrasher: Skate and Destroy",
-      "Evel Knievel",
-      "Grand Theft Auto",
-      "Wild Metal",
-      "Austin Powers: Oh, Behave!",
-      "Austin Powers: Welcome to My Underground Lair!",
-      "Midnight Club: Street Racing",
-      "Smuggler's Run",
-      "Surfing H3O",
-      "Oni",
-      "Grand Theft Auto III",
-      "Smuggler's Run 2: Hostile Territory",
-      "Max Payne",
-      "State of Emergency",
-      "The Italian Job",
-      "Smuggler's Run: Warzones",
-      "Grand Theft Auto: Vice City",
-	  "Midnight Club II",
-	  "Max Payne 2: The Fall of Max Payne",
-	  "Manhunt",
-	  "Red Dead Revolver",
+$(function () {
+	var availableTags = [
+		"Grand Theft Auto: Mission Pack #1 - London 1969",
+		"Grand Theft Auto: Mission Pack #2 - London 1961",
+		"Monster Truck Madness 64",
+		"Grand Theft Auto 2",
+		"Earthworm Jim 3D",
+		"Thrasher: Skate and Destroy",
+		"Evel Knievel",
+		"Grand Theft Auto",
+		"Wild Metal",
+		"Austin Powers: Oh, Behave!",
+		"Austin Powers: Welcome to My Underground Lair!",
+		"Midnight Club: Street Racing",
+		"Smuggler's Run",
+		"Surfing H3O",
+		"Oni",
+		"Grand Theft Auto III",
+		"Smuggler's Run 2: Hostile Territory",
+		"Max Payne",
+		"State of Emergency",
+		"The Italian Job",
+		"Smuggler's Run: Warzones",
+		"Grand Theft Auto: Vice City",
+		"Midnight Club II",
+		"Max Payne 2: The Fall of Max Payne",
+		"Manhunt",
+		"Red Dead Revolver",
 		"Grand Theft Auto Advance",
 		"Grand Theft Auto: San Andreas",
 		"Midnight Club 3: DUB Edition",
@@ -190,8 +216,8 @@ $( function() {
 		"Red Dead Redemption 2",
 		"Red Dead Online",
 		"Grand Theft Auto: The Trilogy – The Definitive Edition",
-    ];
-    $( ".games-search input" ).autocomplete({
-      source: availableTags
-    });
-  } );
+	];
+	$(".games-search input").autocomplete({
+		source: availableTags
+	});
+});
